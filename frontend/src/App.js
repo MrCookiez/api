819e390b046ab837c7b1f5e93c121b0be6673f7b
MrCookiez <theodorosvragkos@gmail.com>
2019-11-09 21:59:03 +0100

    folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Welcome from './pages/Welcome';
+import ProfileUser from './pages/ProfileUser';
+import ProfileOwner from './pages/ProfileOwner';
+import ProfileAdmin from './pages/ProfileAdmin';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -14,7 +16,9 @@ class App extends Component {
                 <Route path='/' component={Home} exact>HOME</Route>
                 <Route path='/login' component={Login} />
                 <Route path='/register' component={Register} />
-                <Route path='/welcome' component={Welcome} />
+                <Route path='/profile-user' component={ProfileUser} />
+                <Route path='/profile-owner' component={ProfileOwner} />
+                <Route path='/profile-admin' component={ProfileAdmin} />
               </Switch>
         </div>
         </BrowserRouter>
