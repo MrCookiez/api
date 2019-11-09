37a473687155668281a6ec98d9d7e8462f9c4d51
MrCookiez <theodorosvragkos@gmail.com>
2019-11-09 21:50:45 +0100

    more routes

---
+++ backend/routes/statistics.js
@@ -0,0 +1,5 @@
+/**
+ * 1. Total income this month/quorter/year (Gross, Net, Fee)
+ * 2. Traffic analytics (Most popular products, less popular products)
+ * 3. Total orders this month/quorter/year (Gross, Net, Fee)
+ */
