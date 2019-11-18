8e2457a37e65463c55bc05a1e900bf70b5be3090
MrCookiez <theodorosvragkos@gmail.com>
2019-11-18 02:35:49 +0100

    hash users password

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,7 +2,7 @@ const service = require('../services/auth');
 const db = require('../config/db');
 
 // Register user
-module.exports = register = (req, res) => {
+module.exports = register = async (req, res) => {
     const user = {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
@@ -10,27 +10,13 @@ module.exports = register = (req, res) => {
         email: req.body.email
     };
 
-    const insertQuery = "INSERT INTO users (firstname, lastname, password, email) VALUES (?, ?, ?, ?)";
-    db.query(insertQuery, [user.firstname, user.lastname, user.password, user.email], (err, result) => {
-        if (err) throw err;
-		res.json('1 record inserted', result);
-    });
-
-    // console.log("Connected!");
-	// const query = "INSERT INTO users (firstname, lastname, password, email) VALUES ('firstnameTEST', 'lastnameTEST', 'passwordTEST', 'email@testemail.com')";
-	// db.query(query, (err, result) => {
-		// if (err) throw err;
-		// console.log("1 record inserted");
-		// console.log(result);
-		// res.json('Done!');
-	// });
-        // if (result) {
-        //     res.redirect('/');
-        // } else {
-        //     res.send('Incorrect firstname and/or Password!');
-        // }
-    // req.session.loggedin = true;
-    // req.session.firstname = firstname;
+    try {
+        service.register(user);
+        if (user) res.send('New user added to the database!');
+        res.redirect('/login');
+    } catch (error) {
+        res.redirect('/register');
+    }
 };
 
 // Login user
--- backend/services/auth.js
+++ backend/services/auth.js
@@ -1,16 +1,28 @@
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
 
 // Register user
 const register = async (user) => {
-    const insertQuery = "INSERT INTO users(firstname, lastname, password, email) values(?, ? , ?, ?)";
-    await db.query(insertQuery, [user.firstname, user.lastname, user.password, user.email]);
+    const insertQuery = "INSERT INTO users (firstname, lastname, password, email) VALUES (?, ?, ?, ?)";
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    await db.query(insertQuery, [user.firstname, user.lastname, hashedPassword, user.email], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
 };
 
-const service = {
-    register,
-};
+// Login user
+// const login = async (user) => {
+//     if (err) throw err;
+//     console.log(result);
+//     // res.send('New user added to the database!');
+// };
+
+const service = { register };
 
 module.exports = service;
+
 // Login user
 // module.exports = login = (req, res) => {
 //     res.body
