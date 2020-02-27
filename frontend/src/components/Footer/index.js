7eaee278c8b38e00ab9678322db01c947971bbbe
MrCookiez <theodorosvragkos@gmail.com>
2020-02-27 20:37:15 +0100

    footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'styled-components/macro';
+import { NavLink } from 'react-router-dom';
 import content from './content';
 import CustomLink from '../CustomLink';
 import styles from './styles';
@@ -7,6 +8,11 @@ import styles from './styles';
 const Footer = () => (
     <div css={styles}>
          <div className='container'>
+            <ul>
+                <li><NavLink to='/contact' activeClassName='active' exact>contact</NavLink></li>
+                <li><NavLink to='/jobs' activeClassName='active' exact>jobs</NavLink></li>
+                <li><NavLink to='/register' activeClassName='active' exact>register</NavLink></li>
+            </ul>
             <p>{content.text}</p>
             <CustomLink
                 label={content.link.label}
